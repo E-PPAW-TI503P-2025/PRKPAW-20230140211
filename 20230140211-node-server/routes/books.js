7b0c5e85bfeb71cf1abc271fac28f17eebe0d28b
Viaasyafrina16 <syafrinavia@gmail.com>
2025-10-11 14:17:24 +0700

    tugas 2

---
+++ 20230140211-node-server/routes/books.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const router = express.Router();
+
+let books = [
+  { id: 1, title: "Book 1", author: "Author 1" },
+  { id: 2, title: "Book 2", author: "Author 2" },
+];
+
+
+router.get("/", (req, res) => {
+  res.json(books);
+});
+
+
+router.get("/:id", (req, res) => {
+  const book = books.find((b) => b.id === parseInt(req.params.id));
+  if (!book) return res.status(404).send("Book not found");
+  res.json(book);
+});
+
+
+router.post("/", (req, res) => {
+  const { title, author } = req.body;
+  if (!title || !author) {
+    return res.status(400).json({ message: "Title and author are required" });
+  }
+  const book = {
+    id: books.length + 1,
+    title,
+    author,
+  };
+  books.push(book);
+  res.status(201).json(book);
+});
+
+
+router.put("/:id", (req, res) => {
+  const bookId = parseInt(req.params.id);
+  const { title, author } = req.body;
+
+  const book = books.find((b) => b.id === bookId);
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  if (!title || !author) {
+    return res.status(400).json({ message: "Title and author are required" });
+  }
+
+  // Update data
+  book.title = title;
+  book.author = author;
+
+  res.json(book);
+});
+
+
+router.delete("/:id", (req, res) => {
+  const bookId = parseInt(req.params.id);
+  const index = books.findIndex((b) => b.id === bookId);
+
+  if (index === -1) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  books.splice(index, 1);
+  res.json({ message: "Book deleted successfully" });
+});
+
+module.exports = router;
---
+++ Tugas/ss/Tugas2.md
@@ -0,0 +1,12 @@
+Tampilan Endpoint Create Book:
+![Tampilan Endpoint Create]![Tampilan Endpoint Create](Create.png)
+
+Tampilan Endpoint Read Book:
+![Tampilan Endpoint Create]![Tampilan Endpoint Read](Read.png)
+
+Tampilan Endpoint Delete Book:
+![Tampilan Endpoint Delete]  ![Tampilan Endpoint Delete](Delete.png)
+
+Tampilan Endpoint Update Book:
+![Tampilan Endpoint Update] ![Tampilan Endpoint Update](Update.png)
+
